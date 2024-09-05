18c4a1ec3394443bb8d59088dd69878ab720f430
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-09-05 14:09:54 +0100

    take 2

--- app/page.test.js
+++ app/page.test.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Home from "./page.js"
+import Home from "./page.js";
 
-
-test('renders Next.js logo', () => {
+test("renders Next.js logo", () => {
   render(<Home />);
-  
+
   // Check if the Next.js logo is rendered
-  const nextLogo = screen.getByAltText('Next.js Logo');
+  const nextLogo = screen.getByAltText("Next.js Logo");
   expect(nextLogo).toBeInTheDocument();
 });
