bd3f9307b5b1e31bd84c3afe6ce3364737e7ba77
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-09-06 13:48:24 +0100

    please work

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Header from "./src/components/components";
 
 export default function Home() {
-  console.error("is this gonna work???");
+  console.error("is this going to work!?")
   return (
     <main className={styles.main}>
       <div className={styles.description}>
